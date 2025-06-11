cf31d235d24801f028925237e681788a80cfb66e
rjuan <rjuan.andrade99@gmail.com>
2025-06-11 14:11:50 -0300

    aluraGame

---
+++ exercicio.js
@@ -0,0 +1,52 @@
+//ex1.1
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'desafio';
+
+//ex1.2
+//function exibirMensagem() {
+//    console.log('botao foi clicado');
+//}
+
+//ex1.3
+//function exibirAlerta() {
+//    alert('eu amo js!');
+//}
+
+//ex1.4
+//function nomeDaCidade(){
+//    let cidade = prompt('Qual é o nome da sua cidade?');
+//    alert(`estive em ${cidade} e gostei muito!`);
+//}
+
+//ex2.1
+//function olaMundo () {
+//    console.log('Olá, mundo!');
+//}
+//exibirOla();
+
+//ex2.2
+//function nomeDoUsuario() {
+//    console.log(`Olá, ${nome}!`);
+//}
+
+//ex2.3
+//function calculadorDobro(numero) {
+//    return numero * 2;
+//}
+//let resultado = calculadorDobro(5);
+//console.log(resultado);
+
+//ex2.4
+//function calcularMedia(a, b, c){
+//    return (a + b + c) / 3;
+    
+//}
+//let media = calcularMedia(4, 7, 10);
+//console.log(media);
+
+//ex2.5
+//function encontrarMaior(a, b) {
+//    return a > b ? a : b;
+//}
+//let maiorNumero = encontrarMaior(15, 9);
+//console.log(maiorNumero);
